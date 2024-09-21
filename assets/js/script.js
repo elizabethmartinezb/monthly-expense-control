8e78e05560ecb98c6d8ed46790b7f4bbf259f4aa
Elizabeth Martinez <marieli.martinezb@gmail.com>
2024-09-21 12:03:40 -0600

    Desafío clase 2: Agregar alerta de gasto y descripción del gasto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,33 @@
 let listaNombresGastos = [];
 let listaValoresGastos = [];
+let listaDescripcionGastos = [];
 
 // Función que se invoca al momento en el que el usuario hace clic en el botón gasto.
 function clickBoton () {
     let nombreGasto = document.getElementById('nombreGasto').value;
-    let valorGasto = document.getElementById('valorGasto').value;
+    let valorGasto = Number(document.getElementById('valorGasto').value);
+    let descripcionGasto = document.getElementById('descripcionGasto').value;
 
     listaNombresGastos.push(nombreGasto);
     listaValoresGastos.push(valorGasto);
+    listaDescripcionGastos.push(descripcionGasto)
     
     actualizarListaGastos();
+    alertaGasto(valorGasto);
 }
 
 function actualizarListaGastos() {
     const listaElementos = document.getElementById('listaDeGastos');
     const totalElementos = document.getElementById('totalGastos');
+
     let htmlLista = '';
     let totalGastos = 0;
 
     listaNombresGastos.forEach((elemento, posicion) => {
         const valorGasto = Number(listaValoresGastos[posicion]);
-        htmlLista += `<li>${elemento} - USD ${valorGasto.toFixed(2)}
-                        <button onclick="eliminarGasto(${posicion});">Eliminar</button></li>`;
-
+        const descripcionGasto = listaDescripcionGastos[posicion];
+        htmlLista += `<li>${elemento} - USD ${valorGasto.toFixed(2)} - ${descripcionGasto}
+                        <button class="update-btn">Actualizar</button></li><button onclick="eliminarGasto(${posicion});">Eliminar</button></li>`;
         // Calculando el total de los gastos
         totalGastos += Number(valorGasto);
     });
@@ -35,6 +40,7 @@ function actualizarListaGastos() {
 function limpiar() {
     document.getElementById('nombreGasto').value = '';
     document.getElementById('valorGasto').value = '';
+    document.getElementById('descripcionGasto').value = '';
 }
 
 function eliminarGasto(posicion) {
@@ -42,5 +48,12 @@ function eliminarGasto(posicion) {
     
     listaNombresGastos.splice(posicion, 1);
     listaValoresGastos.splice(posicion, 1);
+    listaDescripcionGastos.splice(posicion, 1);
     actualizarListaGastos();
+}
+
+function alertaGasto(valorGasto) {
+    if(valorGasto > 150) {
+        alert('¡Cuida tus finanzas! \n¡Has realizado un gasto mayor a 150 USD!')
+    }
 }
